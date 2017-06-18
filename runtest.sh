fb20c076a51b940560ecae9905e34726cb754b56
jasdevProject12 <jasdev.project12@gmail.com>
2017-06-18 23:08:29 +0100

    running e2e test in travis ci I

--- runtest.sh
+++ runtest.sh
@@ -1,7 +1,3 @@
-echo "---------------INSTALLING PROTRACTOR AND PHANTOMJS--------------------"
-npm install phantomjs-prebuilt
-npm install protractor@5.1.2
-webdriver-manager update
 echo "---------Going to angularDesign package------------------------------"
 cd ./packages/angularDesign
 echo "---------INSTALLING DEPENDENCIES------------------------------"
@@ -10,6 +6,6 @@ echo "---------RUNNING UNIT TEST------------------------------"
 npm run test
 echo "---------STARTING DEVELOPMENT SERVER------------------------------"
 npm start &
-sleep 7
+sleep 15
 echo "---------RUNNING E2E TEST------------------------------"
 npm run e2e
