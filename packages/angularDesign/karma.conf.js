10b3028ac887c0c066ee6fa34a886b3665da0bad
jasdevProject12 <jasdev.project12@gmail.com>
2017-06-23 23:09:30 +0100

    fixing unit test setup II

--- packages/angularDesign/karma.conf.js
+++ packages/angularDesign/karma.conf.js
@@ -83,7 +83,7 @@ module.exports = function(config) {
         colors: true,
         logLevel: config.LOG_INFO,
         autoWatch: false,
-        browsers: ['chrome_headless'], // browsers: ['Chrome'] or ['PhantomJS'] or ['chrome_headless']
+        browsers: ['ChromeHeadless'], // browsers: ['Chrome'] or ['PhantomJS'] or ['chrome_headless']
         customLaunchers: {
             chrome_headless: {
                 base: 'Chrome',
